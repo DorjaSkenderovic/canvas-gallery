c88940dfad3b47e4f9422c4cb5a73752209ab40d
DorjaSkenderovic <dorjaskenderovic@gmail.com>
2021-04-03 13:24:18 +0200

    welcome-activity-design

---
+++ app/src/main/java/com/example/canvasgalerija/activities/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.canvasgalerija.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
